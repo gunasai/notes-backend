dd83aafac76255c38410241d7e7ff9e4c54a37cb
gunasai <garapatigunasai@gmail.com>
2023-12-13 12:37:04 -0600

    project structure and added express router

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,8 +13,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -24,11 +24,11 @@ const Note = mongoose.model('Note', noteSchema)
 //     important: true
 // })
 
-Note.find({ important: true }).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
 
 // note.save().then(result => {
